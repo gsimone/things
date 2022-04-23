317c19485c625e7cbe789ba9bcf984ae57b6b7e3
Gianmarco <gianmarcosimone89@gmail.com>
2022-04-23 14:42:51 +0200

    .

--- packages/things/src/index.ts
+++ packages/things/src/index.ts
@@ -2,5 +2,4 @@ export * from "@gsimone/bitmask";
 export * from "@gsimone/leva-plugin-bitmask";
 export * from "@gsimone/r3f-layers";
 export * from "@gsimone/smoothdamp";
-export * from "@gsimone/three-catenary";
 export * from "@gsimone/three-raycaster-helper";
